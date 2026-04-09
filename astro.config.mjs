5db42f5c810780342dff4322ee17108504032b8c
bcamposonepark <helpdeskwetrade@gmail.com>
2026-04-09 13:14:08 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config'
-import vue from '@astrojs/vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  integrations: [vue()],
   vite: {
     plugins: [tailwindcss()],
   },
